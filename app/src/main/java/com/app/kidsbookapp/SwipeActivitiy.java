4d340a9160e1c9b48f88e50a35122b7a5d5ccd33
Krishna <softeng.krishna@gmail.com>
2015-07-20 23:15:36 +0530

    hjm

--- app/src/main/java/com/app/kidsbookapp/SwipeActivitiy.java
+++ app/src/main/java/com/app/kidsbookapp/SwipeActivitiy.java
@@ -261,6 +261,7 @@ public class SwipeActivitiy extends Activity implements OnClickListener {
 
 
 
+
 						if(category.equals("alphabet")){
 						callAlphabet();
 					}else if(category.equals("number")){
